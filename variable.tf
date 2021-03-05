6f067150b16e549d4f73596f03af25615746ed3a
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-05 10:15:27 -0300

    Adding variable file

---
+++ variable.tf
@@ -0,0 +1,17 @@
+# Variable to create a ec2 in N.Virginia
+variable "aws_region"{
+  default = "us-east-1" 
+}
+
+# Variable to get Image free tier
+variable "ami_web" {
+  type = map
+  default = {
+    "us-east-1" = "ami-0ac80df6eff0e70b5"
+  }
+}
+
+# How many instances is going up
+variable "aws_count_instante"{
+  default = 2
+}
