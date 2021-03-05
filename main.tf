2a38ca755551b21e7486ffbb27ad41bbde7e7552
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-05 10:13:22 -0300

    Adding main file

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Creating a provider aws as default
+provider "aws" {
+  region  = var.aws_region
+}
+
+# Creating a new instance of the latest Ubuntu 14.04 on an
+resource "aws_instance" "web" {
+  ami           = lookup(var.ami_web, var.aws_region)
+  instance_type = "t2.micro"
+  count         = var.aws_count_instante
+  user_data     =  file("userdata.yaml")
+
+  tags = {
+
+    Name = "Web-${count.index + 1}"
+  }
+
+}
+
+# This output resource get the public ip in the end of terraform command
+output "ip-web" {
+  value = "${aws_instance.web.*.public_ip}"
+ }
