dd82d9df29751bf9f14256c77c6301dfd3359bfc
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-05 13:30:26 -0300

    changes network files

--- network.tf
+++ network.tf
@@ -7,39 +7,3 @@ resource "aws_vpc" "vpc_main" {
     Name = "vpc_main"
   }
 }
-
-resource "aws_subnet" "subnet_main_100" {
-  vpc_id     = aws_vpc.vpc_main.id
-  cidr_block = "10.10.100.0/24"
-
-  tags = {
-    Name = "subnet_main_100"
-  }
-}
-
-resource "aws_internet_gateway" "internet_gatway" {
-  vpc_id = aws_vpc.vpc_main.id
-
-  tags = {
-    Name = "internet_gatway"
-  }
-}
-
-
-resource "aws_route_table" "route" {
-  vpc_id = aws_vpc.vpc_main.id
-
-  route {
-    cidr_block = "10.10.0.0/16"
-    gateway_id = aws_internet_gateway.vpc_main.id
-  }
-
-  #route {
-  #  ipv6_cidr_block        = "::/0"
-  #  egress_only_gateway_id = aws_egress_only_internet_gateway.foo.id
-  #}
-
-  tags = {
-    Name = "main"
-  }
-}
