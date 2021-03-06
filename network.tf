18a1f068b291d2eb98d855ea16426f8921071b15
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-06 09:52:05 -0300

    add subnets config

--- network.tf
+++ network.tf
@@ -7,3 +7,23 @@ resource "aws_vpc" "vpc_main" {
     Name = "vpc_main"
   }
 }
+
+# Creating a subnet A and make association with vpc_main
+resource "aws_subnet" "subnet_A" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "10.10.100.0/24"
+
+  tags = {
+    Name = "subnet_A"
+  }
+}
+
+# Creating a subnet B and make association with vpc_main
+resource "aws_subnet" "subnet_B" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "10.10.200.0/24"
+
+  tags = {
+    Name = "subnet_B"
+  }
+}
