6c838c99e7dc7c67ca3ce3334c2d2b91cd7e1a30
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-07 10:16:28 -0300

    add public ip as true

--- network.tf
+++ network.tf
@@ -2,9 +2,35 @@
 resource "aws_vpc" "vpc_main" {
   cidr_block       = "10.10.0.0/16"
   instance_tenancy = "default"
+  enable_dns_hostnames = true
 
   tags = {
     Name = "vpc_main"
+  }
+}
+
+#Create internet gateway
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc_main.id
+  
+
+  tags = {
+    Name = "gateway"
+  }
+}
+
+# Creating a subnet and make association with vpc_main and availability zones
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc_main.id
+  count      = length(var.subnet_cidr)
+  cidr_block = var.subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = "true"
+
+  depends_on = [aws_internet_gateway.gateway]
+
+  tags = {
+    Name = "subnet-${count.index}"
   }
 }
 
@@ -21,34 +47,9 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-# Creating a subnet and make association with vpc_main
-resource "aws_subnet" "subnet" {
-  vpc_id     = aws_vpc.vpc_main.id
-  count      = length(var.subnet_cidr)
-  cidr_block = var.subnet_cidr[count.index]
-  availability_zone = var.azs[count.index]
-  
-  tags = {
-    Name = "subnet-${count.index}"
-  }
-}
-
-resource "aws_internet_gateway" "gateway" {
-  vpc_id = aws_vpc.vpc_main.id
-
-  tags = {
-    Name = "gateway"
-  }
-}
-
+#Create association among route table, gateway and subnets
 resource "aws_route_table_association" "route_table_association_subnet" {
   count = length(var.subnet_cidr)
   subnet_id  = element(aws_subnet.subnet.*.id, count.index)
   route_table_id = aws_route_table.route_table.id
 }
-
-#resource "aws_route_table_association" "route_table_association_gateway" {
-#
-#  gateway_id     = aws_internet_gateway.gateway.id
-#  route_table_id = aws_route_table.route_table.id
-#}
