a974ed405ac8d67f20878b658af90aa508ee51ba
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-06 23:40:42 -0300

    route table

--- network.tf
+++ network.tf
@@ -12,14 +12,9 @@ resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc_main.id
 
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gateway.id
   }
-
-  #route {
-  #  ipv6_cidr_block        = "::/0"
-  #  egress_only_gateway_id = aws_egress_only_internet_gateway.gateway.id
-  #}
 
   tags = {
     Name = "route_table"
@@ -46,12 +41,14 @@ resource "aws_internet_gateway" "gateway" {
   }
 }
 
-resource "aws_route_table_association" "route_table_association" {
-  subnet_id      = aws_subnet.subnet.id
+resource "aws_route_table_association" "route_table_association_subnet" {
+  count = length(var.subnet_cidr)
+  subnet_id  = element(aws_subnet.subnet.*.id, count.index)
   route_table_id = aws_route_table.route_table.id
 }
 
-resource "aws_route_table_association" "b" {
-  gateway_id     = aws_internet_gateway.gateway.id
-  route_table_id = aws_route_table.route_table.id
-}
+#resource "aws_route_table_association" "route_table_association_gateway" {
+#
+#  gateway_id     = aws_internet_gateway.gateway.id
+#  route_table_id = aws_route_table.route_table.id
+#}
