941bf8ef974806ff1686fe30eae13515515ef2e6
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-06 21:58:04 -0300

    add route table

--- network.tf
+++ network.tf
@@ -8,22 +8,50 @@ resource "aws_vpc" "vpc_main" {
   }
 }
 
-# Creating a subnet A and make association with vpc_main
-resource "aws_subnet" "subnet_A" {
-  vpc_id     = aws_vpc.vpc_main.id
-  cidr_block = "10.10.100.0/24"
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc_main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  #route {
+  #  ipv6_cidr_block        = "::/0"
+  #  egress_only_gateway_id = aws_egress_only_internet_gateway.gateway.id
+  #}
 
   tags = {
-    Name = "subnet_A"
+    Name = "route_table"
   }
 }
 
-# Creating a subnet B and make association with vpc_main
-resource "aws_subnet" "subnet_B" {
+# Creating a subnet and make association with vpc_main
+resource "aws_subnet" "subnet" {
   vpc_id     = aws_vpc.vpc_main.id
-  cidr_block = "10.10.200.0/24"
+  count      = length(var.subnet_cidr)
+  cidr_block = var.subnet_cidr[count.index]
+  availability_zone = var.azs[count.index]
+  
+  tags = {
+    Name = "subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.vpc_main.id
 
   tags = {
-    Name = "subnet_B"
+    Name = "gateway"
   }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.subnet.*.id
+  route_table_id = aws_route_table.route_table.*.id
+}
+
+resource "aws_route_table_association" "b" {
+  gateway_id     = aws_internet_gateway.gateway.*.id
+  route_table_id = aws_route_table.route_table.*.id
 }
