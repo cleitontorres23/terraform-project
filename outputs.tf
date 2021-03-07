3ef98ba351991a2a6c99776d897127db4b0c1a6e
cleitontorres23 <cleiton.torres23@gmail.com>
2021-03-07 10:27:08 -0300

    add output for public ip

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
 # Output variable definitions
-output "ec2_instance_public_ips" {
-  description = "Public IP addresses of EC2 instances"
+output "ec2_instance_private_ips" {
+  description = "Private IP addresses of EC2 instances"
+  value = aws_instance.webservice.*.private_ip
+ }
+
+ output "ec2_instance_public_ips" {
+  description = "Private IP addresses of EC2 instances"
   value = aws_instance.webservice.*.public_ip
  }
 
